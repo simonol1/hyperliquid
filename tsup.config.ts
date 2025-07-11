73b3bd8d4cf5ebfed96b544655faf074cf2aa64e
Simon O'Leary <simon.oleary@kindredgroup.com>
2025-07-11 20:43:44 +1000

    build use mjs

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,12 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: {
     bot: 'src/bots/index.ts',
-    orchestrator: 'src/orchestrator/index.ts'
+    orchestrator: 'src/orchestrator/index.ts',
   },
   outDir: 'dist',
-  format: 'cjs',
-  target: 'node18',
+  format: 'esm',
   splitting: false,
   sourcemap: true,
   clean: true,
+  outExtension: () => ({ js: '.mjs' }),
 });
