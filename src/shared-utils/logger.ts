3b17defad46f1aa6bd66f5958e8bf279b6a6aa24
Simon O'Leary <simon.oleary@kindredgroup.com>
2025-07-26 10:20:43 +1000

    use env var for logs levels

--- src/shared-utils/logger.ts
+++ src/shared-utils/logger.ts
@@ -17,7 +17,7 @@ if (!fs.existsSync(logsDir)) {
 const logFilePath = path.join(logsDir, `trades_${today}.log`);
 
 // === Log level (env or default)
-const LOG_LEVEL = 'debug'
+const LOG_LEVEL = process.env.LOG_LEVEL ?? 'info';
 
 export const logger = pino(
   {
